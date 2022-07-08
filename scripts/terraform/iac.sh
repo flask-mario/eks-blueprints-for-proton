ebb00ed81e81bba08c0ca52b5ef878a15427b41c
flask-mario <flask@mz.co.kr>
2022-07-08 01:34:25 +0000

    modify iac script

--- scripts/terraform/iac.sh
+++ scripts/terraform/iac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 terraform init
-TF_VAR_github_org=$GITHUB_USER \
-TF_VAR_github_repo=eks-blueprints-for-proton \
-   terraform apply -auto-approve
+TF_VAR_github_org=$GITHUB_USER TF_VAR_github_repo=$GITHUB_REPO terraform apply -auto-approve
